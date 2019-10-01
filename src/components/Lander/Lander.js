76e1a2a0807c8b2a9ef90ff2410a545cf4830e17
skylar siggard <siggardskylar@gmail.com>
2019-10-01 10:26:53 -0600

    toggle login in works

--- src/components/Lander/Lander.js
+++ src/components/Lander/Lander.js
@@ -4,13 +4,51 @@ export default class Lander extends Component {
     constructor() {
         super()
         this.state = {
-
+            register: false,
+            email: '',
+            password: '',
+            password2: ''
         }
+    }
+    toggleChange = () => {
+        this.setState({
+            register: !this.state.register
+        })
+    }
+    handleChange = (e, key) => {
+        this.setState({
+            [key]: e.target.value
+        })
     }
     render() {
         return(
         <div className="lander">
             Lander
+            {!this.state.register
+            ? (
+            <div className="login">
+                <div>email</div>
+                <input onChange={(e) => this.handleChange(e, 'email')} type="text" placeholder='email'/>
+                <div>password</div>
+                <input onChange={(e) => this.handleChange(e, 'password')} type="text" placeholder='password'/>
+                <div>
+                    <button onClick={() => this.toggleChange()}>Register</button>
+                    <button>Login</button>
+                </div>
+            </div>
+            ) : (
+                <div className="login">
+                <div>email</div>
+                <input onChange={(e) => this.handleChange(e, 'email')} type="text" placeholder='email'/>
+                <div>password</div>
+                <input onChange={(e) => this.handleChange(e, 'password')} type="text" placeholder='password'/>
+                <input onChange={(e) => this.handleChange(e, 'password2')} type="text" placeholder='password'/>
+                <div>
+                    <button onClick={() => this.toggleChange()}>Back to Login</button>
+                    <button>Register</button>
+                </div>
+            </div>
+            )}
         </div>
         )
     }
