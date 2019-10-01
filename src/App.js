8cef540fafbf9cad76c71fe0877afa3bf30ee5be
skylar siggard <siggardskylar@gmail.com>
2019-10-01 09:30:32 -0600

    basic set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Lander from './components/Lander/Lander'
+import Header from './components/Header/Header'
+import Create from './components/Create/Create'
+import List from './components/List/List'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lander/>
+      <Header/>
+      <Create/>
+      <List/>
     </div>
   );
 }
---
+++ src/components/Create/Create.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+export default class Create extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+        <div className="create">
+            Create
+        </div>
+        )
+    }
+}
