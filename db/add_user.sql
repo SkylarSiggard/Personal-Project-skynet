dc1ddce3cfb576991fa90335d7478c544caac91c
skylar siggard <siggardskylar@gmail.com>
2019-10-01 16:28:00 -0600

    db is somewhat set up

---
+++ db/add_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_info (user_email, phone_number) 
+VALUES (${email}, ${number});
+
---
+++ db/seed.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS user_info;
+DROP TABLE IF EXISTS user_events;
+DROP TABLE IF EXISTS user_login_info;
+
+CREATE TABLE user_info (
+	user_id SERIAL PRIMARY KEY,
+	user_email text,
+	phone_number varchar(10)
+);
+CREATE TABLE user_login_info (
+    user_login_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user_info(user_id),
+    hash TEXT
+);
+CREATE TABLE user_events (
+	event_id SERIAL PRIMARY KEY,
+	user_id INT REFERENCES user_info(user_id),
+	title varchar(30),
+	description varchar(300),
+	starting_day date,
+	ending_day date,
+	starting_time time,
+	ending_time time
+);
+select * from user_info;
+select * from user_events;
+select * from user_login_info;
